1f0be5940c4ac898b0e79eb5d59d14cb711a9664
Chris Swithinbank <swithinbank@gmail.com>
2024-03-15 22:43:00 +0100

    multiline content

--- db/config.ts
+++ db/config.ts
@@ -2,12 +2,12 @@ import { column, defineDb, defineTable } from 'astro:db';
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {
-    Article: defineTable({
-      columns: {
-        title: column.text(),
-        content: column.text(),
-      },
-    }),
-  },
+	tables: {
+		Article: defineTable({
+			columns: {
+				title: column.text(),
+				content: column.text({ multiline: true }),
+			},
+		}),
+	},
 });
