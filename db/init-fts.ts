88e2269c6f9b242208ea5a7a2c626265a3255ffa
Chris Swithinbank <swithinbank@gmail.com>
2024-03-16 19:51:20 +0100

    Batch in init-fts

--- db/init-fts.ts
+++ db/init-fts.ts
@@ -1,18 +1,18 @@
 import { Article, db, sql } from 'astro:db';
 
 export default async function initFts() {
-	// Clean up any existing table if it exists.
-	await db.run(sql`DROP TABLE IF EXISTS UDHRArticles`);
-	// Create virtual full-text search table.
-	await db.run(
-		sql`CREATE VIRTUAL TABLE UDHRArticles USING FTS5(
-			title UNINDEXED,
-			content,
-			tokenize = 'porter unicode61 remove_diacritics 2'
-		);`
-	);
-	// Insert content into the database.
-	await db.run(
-		sql`INSERT INTO UDHRArticles (title, content) SELECT title, content FROM ${Article};`
-	);
+	await db.batch([
+		// Clean up any existing table if it exists.
+		db.run(sql`DROP TABLE IF EXISTS UDHRArticles`),
+		// Create virtual full-text search table.
+		db.run(
+			sql`CREATE VIRTUAL TABLE UDHRArticles USING FTS5(
+				title UNINDEXED,
+				content,
+				tokenize = 'porter unicode61 remove_diacritics 2'
+			);`
+		),
+		// Insert content into the database.
+		db.run(sql`INSERT INTO UDHRArticles (title, content) SELECT title, content FROM ${Article};`),
+	]);
 }
